8e6121f642dc32cfd71579018f60f231bef04f56
Inna Starovytska <starovitckayainna1@gmail.com>
2024-10-27 22:39:41 +0200

    commit-2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { setupServer } from "./server.js";
+import { initMongoConnection } from './db/initMongoConnection.js';
+
 
 const boostrap = async () => {
+   await initMongoConnection();
     setupServer();
-}
+};
+
 boostrap();
--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
 import pino from "pino-http";
+import { env } from '../src/utils/env.js';
+import * as contactServices from '../src/services/contacts.js';
 
 
 export const setupServer = () => {
+
     const app = express();
 
     app.use(cors());
@@ -12,24 +15,48 @@ export const setupServer = () => {
             target: "pino-pretty"
         }
     });
-    // app.use(logger);
+    app.use(logger);
 
-    app.get("/", (req, res) => {
+    app.get("/contacts", async (req, res) => {
+        const data = await contactServices.getContacts();
         res.json({
-            message: "Start project"
-        })
+            status: 200,
+            message: "Successfully found contacts!",
+            data,
+        });
     });
+    
+    app.get('/contacts/:id', async (req, res) => {
+    const { id } = req.params;
+    const data = await contactServices.getContactById(id);
+
+    if (!data) {
+      return res.status(404).json({
+        status: 404,
+        message: 'Contact not found',
+      });
+    }
+
+    res.json({
+      status: 200,
+      message: `Successfully found contact with id ${id}!`,
+      data,
+    });
+    });
+    
     app.use((req, res) => {
         res.status(404).json({
-            message: `${req.url} not found`
-        })
+            message: `Not found`
+        });
     });
-    app.use((req, res) => {
+    app.use((error, req, res, next) => {
         res.status(500).json({
             message: error.message,
-        })
+        });
     });
 
-    app.listen(3000, () => console.log("Server running on 3000 PORT"));
+    const port = Number(env("PORT", 3001));
+
+    app.listen(port, () => console.log(`Server is running on ${port} PORT`));
     
-}
+};
