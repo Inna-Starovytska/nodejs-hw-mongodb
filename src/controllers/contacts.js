809d8d9bc9b058339c9fc93da090915a82db4448
Inna Starovytska <starovitckayainna1@gmail.com>
2024-11-01 22:00:59 +0200

    commit-1

---
+++ src/controllers/contacts.js
@@ -0,0 +1,33 @@
+import createHttpError from 'http-errors';
+
+import * as contactServices from '../services/contacts.js';
+
+
+export const getContactsController = async (req, res, next) => {
+  
+        const data = await contactServices.getContacts();
+        res.json({
+            status: 200,
+            message: "Successfully found contacts!",
+            data,
+        });  
+};
+
+
+export const getContactsByIdController = async (req, res,) => {
+   
+        const { id } = req.params;
+    const data = await contactServices.getContactById(id);
+
+        if (!data) {
+            throw createHttpError(404, `Contact with id=${id} not found`);
+    }
+
+    res.json({
+        status: 200,
+        message: `Successfully found contact with id ${id}!`,
+        data,
+    });
+    
+
+};
